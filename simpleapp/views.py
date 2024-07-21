d4d34f459a39c416122102a0a9c1959e2df81f40
Danil <danilka780@gmail.com>
2024-07-21 16:51:19 +0600

    first commit

---
+++ simpleapp/views.py
@@ -0,0 +1,11 @@
+# Импортируем класс, который говорит нам о том,
+# что в этом представлении мы будем выводить список объектов из БД
+from django.views.generic import ListView
+from .models import Product
+
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    template_name = 'products.html'
+    context_object_name = 'products'
